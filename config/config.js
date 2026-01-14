135c1243921c2b8f125051750eac3a4cf486fa66
amjadkhanniazi <amjadkhanniazi010@gmail.com>
2026-01-14 10:04:32 +0500

    changes made to connection string(mongoURL) in config file

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-    mongoURL: `mongodb+srv://${process.env.username}:${process.env.password}@freecluster.sui3ma0.mongodb.net/doc_clinic`,
+    mongoURL: `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@freecluster.sui3ma0.mongodb.net/doc_clinic`,
     options: {
         useNewUrlParser: true,
         useUnifiedTopology: true
