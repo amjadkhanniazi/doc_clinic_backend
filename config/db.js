e7b3eb4550e422366b13813c195a4ee248fc717b
amjadkhanniazi <amjadkhanniazi010@gmail.com>
2026-01-14 10:39:24 +0500

    config file deleted, connection string in .env file and directly imported to db.js file, cors policy added to server.js and vercel config file added.

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
-import config from "./config.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 async function connectDB() {
-    const client = mongoose.connect(config.mongoURL);
+    const client = mongoose.connect(process.env.MONGO_URL);
     try{
         await client;
         console.log("Database connected successfully");
--- server.js
+++ server.js
@@ -11,9 +11,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
 connectDB();
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
 
 // Routes
 app.use('/api/auth', authRoutes);
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
