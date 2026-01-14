c3f10312ecfcc16fa4d44848db6537770aec5432
amjadkhanniazi <amjadkhanniazi010@gmail.com>
2026-01-14 11:10:31 +0500

    db connection added to server.js file, tested if it works here, not working with db.js

--- server.js
+++ server.js
@@ -3,10 +3,21 @@ import cors from 'cors';
 import connectDB from './config/db.js';
 import 'dotenv/config.js';
 import authRoutes from './routes/auth_route.js';
+import mongoose from 'mongoose';
+
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URL);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+
 
 const app = express();
-
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
@@ -21,7 +32,7 @@ app.use('/api/auth', authRoutes);
 
 
 export default app;
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
