dfb5fd7de0dc2201b8a3a88a6ed0a78d19c793c3
amjadkhanniazi <amjadkhanniazi010@gmail.com>
2026-01-14 10:54:10 +0500

    console.log method added to auth_routes for debugging purpose

--- routes/auth_route.js
+++ routes/auth_route.js
@@ -7,11 +7,13 @@ const router = express.Router();
 router.post('/register', async (req, res) => {
     const { first_name, last_name, specialization, phone, email, password, licence_number} = req.body;
     try {
+        console.log("try block in register route started");
         const newDoctor = new doctors({ first_name, last_name, specialization, phone, email, password, licence_number });
         await newDoctor.save();
         res.status(201).json({ message: 'Doctor registered successfully' });
     }
     catch (error) {
+        console.log("catch block in register route started");
         res.status(500).json({ message: 'Server Error', error: error.message });
     }
 });
