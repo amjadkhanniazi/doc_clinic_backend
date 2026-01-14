f591c57acc2b4d2b53b39a0bec078a4a548649fd
amjadkhanniazi <amjadkhanniazi010@gmail.com>
2026-01-14 11:02:08 +0500

    app.listen commented and export default added to server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cors({
 app.use('/api/auth', authRoutes);
 
 
-
+export default app;
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
